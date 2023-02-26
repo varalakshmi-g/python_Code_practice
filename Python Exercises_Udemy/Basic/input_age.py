bd2f96093c0b3027f2487adafd909e78ebcd82ad
Nagacharan <Nagacharan4286@gmail.com>
2023-02-27 01:34:44 +0530

    adding python code exercises

--- Python Exercises_Udemy/Basic/input_age.py
+++ Python Exercises_Udemy/Basic/input_age.py
@@ -2,3 +2,6 @@
 Create a program that asks the user to enter their name and their age.
 Print out a message addressed to them that tells them the year that they will turn 100 years old.
 """
+#solution
+
+input("Enter your age: ")
